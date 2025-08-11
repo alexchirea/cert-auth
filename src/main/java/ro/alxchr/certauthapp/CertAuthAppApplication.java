065e5bf39f065e23531452089c91e9006a86db88
Alex Chirea <ac.chirea@gmail.com>
2025-08-11 16:24:52 +0300

    first commit

---
+++ src/main/java/ro/alxchr/certauthapp/CertAuthAppApplication.java
@@ -0,0 +1,13 @@
+package ro.alxchr.certauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertAuthAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertAuthAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/alxchr/certauthapp/CertAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.alxchr.certauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertAuthAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
